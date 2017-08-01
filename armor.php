749aab3e535b56a48d1b8e117c3942a625c376a9
Nikolay Mihaylov <nmmm@nmmm.nu>
2017-08-01 13:00:59 +0300

    fix for arrays

--- armor.php
+++ armor.php
@@ -1,18 +1,22 @@
 <?
 /*
- * PHP Armor 1.2
- * Copyleft 2014.04, Nikolay Mihaylov
+ * PHP Armor 1.2.2017.08
+ * Copyleft 2014.03, Nikolay Mihaylov
  * History:
  *      2014.03 - initial version
  *      2014.04 - virtualized as a function, to prevent registered variables overwrite.
  *      2015.02 - add fancybox fix.
  *      2015.03 - add nginx X_REAL_IP support.
+ *      2017.08 - fix array parameters
  */
 
 function armor_1234567890_abc(){
 	$permited_proxy = array("127.0.0.1");
 
 	foreach($_REQUEST as $key => $data){
+		if (is_array($data))
+			continue;
+
 		$data = strtolower($data);
 
 		if (strpos($data, "base64_") !== false)
