b1225d88185da09c06cfd8389dbc9d436ee823b3
Nikolay Mihaylov <nmmm@nmmm.nu>
2017-10-02 10:28:54 +0300

    Update armor.php

--- armor.php
+++ armor.php
@@ -1,22 +1,19 @@
 <?
 /*
- * PHP Armor 1.2.2017.08
- * Copyleft 2014.03, Nikolay Mihaylov
+ * PHP Armor 1.2
+ * Copyleft 2014.04, Nikolay Mihaylov
  * History:
  *      2014.03 - initial version
  *      2014.04 - virtualized as a function, to prevent registered variables overwrite.
  *      2015.02 - add fancybox fix.
  *      2015.03 - add nginx X_REAL_IP support.
- *      2017.08 - fix array parameters
+ *      2016.10 - add CF HTTPS support.
  */
 
 function armor_1234567890_abc(){
 	$permited_proxy = array("127.0.0.1");
 
 	foreach($_REQUEST as $key => $data){
-		if (is_array($data))
-			continue;
-
 		$data = strtolower($data);
 
 		if (strpos($data, "base64_") !== false)
@@ -34,8 +31,19 @@ function armor_1234567890_abc(){
 		exit;
 
 	// fix REMOTE_ADDR
-	if (isset($_SERVER["HTTP_X_REAL_IP"]) && in_array($_SERVER["REMOTE_ADDR"], $permited_proxy))
+	if (isset($_SERVER["HTTP_X_REAL_IP"])) // && in_array($_SERVER["REMOTE_ADDR"], $permited_proxy))
 		$_SERVER["REMOTE_ADDR"] = $_SERVER["HTTP_X_REAL_IP"];
+
+	if (isset($_SERVER["HTTP_CF_CONNECTING_IP"]))
+		$_SERVER["REMOTE_ADDR"] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+
+	// SSL ceritificate
+	foreach( array( 'HTTP_CF_VISITOR', 'HTTP_X_FORWARDED_PROTO' ) as $option ) {
+		if ( isset( $_SERVER[ $option ] ) && ( strpos( $_SERVER[ $option ], 'https' ) !== false ) ) {
+			$_SERVER[ 'HTTPS' ] = 'on';
+			break;
+		}
+	}
 }
 
 armor_1234567890_abc();
